0be60ca03e317af00d39073d3a1889f4c0843af8
Pedro Guerra <pguerra@cambridgeassociates.com>
2022-08-31 20:19:27 -0300

    otimizado os testes de paginacao

--- cypress/e2e/ui/conexaoQA_paginacao.cy.js
+++ cypress/e2e/ui/conexaoQA_paginacao.cy.js
@@ -1,50 +1,44 @@
 describe('Paginação QAs', () => {
+
+    const paginacao = () => cy.get('.paginationBttns li')
+    const URL_API = '/api/profile'
+    const URL_METHOD = 'GET'
+    const validarPaginas = (resultado) => {
+        paginacao()
+            .should('have.length', resultado.length)
+            .each((el, index) => {
+
+                cy.wrap(el)
+                    .should('have.text', resultado[index])
+            })
+    }
     
     it('valida 7 perfis', () => {
         
-        cy.intercept('GET', '/api/profile', { fixture: 'paginacao_7_usuarios.json' })
+        cy.intercept(URL_METHOD, URL_API, { fixture: 'paginacao_7_usuarios.json' })
             .as('perfis')
 
         cy.visit('/perfis')
             .wait('@perfis')
 
-        cy.get('.paginationBttns li')
+        paginacao()
             .should('not.exist')
     })
 
-    it('valida 8 perfis', () => {
-        cy.intercept('GET', '/api/profile', { fixture: 'paginacao_8_usuarios.json' })
-            .as('perfis')
+    ;[
+        { fixture: 'paginacao_8_usuarios', resultadoEsperado: ['<', '1', '2', '>'] },
+        { fixture: 'paginacao_63_usuarios', resultadoEsperado: ['<', '1', '2', '3', '4', '5', '6', '7', '8', '9', '>'] },
+        { fixture: 'paginacao_64_usuarios', resultadoEsperado: ['<', '1', '2', '3', '4', '5', '6', '...', '8', '9', '10', '>'] },
+    ].forEach(({ fixture, resultadoEsperado }) => {
 
-        cy.visit('/perfis')
-            .wait('@perfis')
+        it(`validar a ${fixture}`, () => {
+            cy.intercept(URL_METHOD, URL_API, { fixture })
+                .as('perfis')
 
-        const RESULTADO_ESPERADO = ['<', '1', '2', '>']
+            cy.visit('/perfis')
+                .wait('@perfis')
 
-        cy.get('.paginationBttns li')
-            .should('have.length', RESULTADO_ESPERADO.length)
-            .each((el, index) => {
-
-                cy.wrap(el)
-                    .should('have.text', RESULTADO_ESPERADO[index])
-            })
-    })
-
-    it('valida 63 perfis', () => {
-        cy.intercept('GET', '/api/profile', { fixture: 'paginacao_63_usuarios.json' })
-            .as('perfis')
-
-        cy.visit('/perfis')
-            .wait('@perfis')
-
-        const RESULTADO_ESPERADO = ['<', '1', '2', '3', '4', '5', '6', '7', '8', '9', '>']
-
-        cy.get('.paginationBttns li')
-            .should('have.length', RESULTADO_ESPERADO.length)
-            .each((el, index) => {
-
-                cy.wrap(el)
-                    .should('have.text', RESULTADO_ESPERADO[index])
-            })
+            validarPaginas(resultadoEsperado)
+        })
     })
 })
